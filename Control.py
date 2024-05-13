a00d778dd512c7cd7264541b40239266b82f494d
Fernanda Perialdo <nandaperialdo@gmail.com>
2024-05-13 15:47:30 -0300

    Terceiro Commit

---
+++ Control.py
@@ -0,0 +1,55 @@
+from Exercicios import Exercicios
+
+class Control:
+    def __init__(self):
+        self.opcao = -1
+        self.exer = Exercicios()
+
+    def coletar1(self):
+        self.exer.num = int(input("Informe um numero: "))
+
+    def coletar2(self):
+        self.exer.num1 = int(input("Informe o primeiro numero: "))
+        self.exer.num2 = int(input("informe o segundo numero: "))
+
+    def menu(self):
+        self.opcao = int(input("----- Menu ------\n\n" +
+                "\n0. Sair" +
+                "\n1. Somar" +
+                "\n2. Subtrair" +
+                "\n3. Dividir" +
+                "\n4. Multiplicar" +
+                "\n5. Contar até 10" +
+                "\n6. Imprima os números pares de 1 a 20" +
+                "\n7. Soma dos números de 1 a 100" +
+                "\n8. Imprima os múltiplos de 5 de 1 a 50" +
+                "\nEscolha uma das opções acima: "))
+
+    def operacao(self):
+        while(self.opcao != 0):
+            self.menu()#mostrar menu
+            if(self.opcao == 0):
+                print("Obrigado!")
+            elif(self.opcao == 1):
+                self.coletar2()
+                print(self.exer.somar())
+            elif(self.opcao == 2):
+                self.coletar2()
+                print(self.exer.subtrair())
+            elif(self.opcao == 3):
+                self.coletar2()
+                print(self.exer.dividir())
+            elif(self.opcao == 4):
+                self.coletar2()
+                print(self.exer.multiplicar())
+            elif(self.opcao == 5):
+                print(self.exer.contarDez())
+            elif(self.opcao == 6):
+                print(self.exer.paresVinte())
+            elif(self.opcao == 7):
+                print(self.exer.somarCem())
+            elif(self.opcao == 8):
+                print(self.exer.multCinco())
+            else:
+                print("Código escolhido nao é valido")
+
---
+++ Exercicios.py
@@ -0,0 +1,58 @@
+class Exercicios:
+    def __init__(self):
+        # Construtor, quando nao precisar delcarar as variaveis, usar pass
+        self.num = 0
+        self.num1 = 0
+        self.num2 = 0
+        self.i = 0
+        self.resultado = 0
+
+    def somar(self):
+        return f"{self.num1} + {self.num2} = {self.num1 + self.num2}"
+
+    def subtrair(self):
+        return f"{self.num1} - {self.num2} = {self.num1 - self.num2}"
+
+    def dividir(self):
+        if(self.num2 == 0):
+            return "Impossivel dividir por zero"
+        else:
+            return f"{self.num1} / {self.num2} = {self.num1 / self.num2}"
+
+    def multiplicar(self):
+        return f"{self.num1} * {self.num2} = {self.num1 * self.num2}"
+
+    def contarDez(self):
+        self.i = 1
+        for self.i in range (1, 11):
+            print(self.i)
+
+    def paresVinte(self):
+        self.i = 1
+        for self.i in range(1, 21):
+            if self.i % 2 == 0:
+                print(self.i)
+
+    def somarCem(self):
+        self.i = 0
+        for self.i in range(1, 101):
+            self.resultado += self.i
+            print(self.resultado)
+
+    def multCinco(self):
+        self.i = 1
+        for self.i in range(1, 51):
+            if self.i % 5 == 0:
+                print(self.i)
+
+
+
+
+
+
+
+
+
+
+
+
